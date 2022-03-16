7c9c868519f557dad26c1ac69b6c9b6c438942c7
michelvasconcelos <michelvasconcelos@hotmail.com>
2022-03-16 00:23:44 -0300

    Corrigido split dt_inicio e baixa corretamente .csv com dados mais recentes

--- WsFXBacen.py
+++ WsFXBacen.py
@@ -4,9 +4,13 @@ from datetime import datetime, timedelta
 
 url_download = "/ptax_internet/consultaBoletim.do?method=gerarCSVFechamentoMoedaNoPeriodo&ChkMoeda=61&DATAINI=08/02/2022&DATAFIM=07/03/2022"
 url_base = url_download.split('ChkMoeda')[0]
+currency = input('Digite o código da moeda (USD: 61 ou EUR: 222): ')
+if currency == '222':
+    curr_name = 'Euro'
+elif currency == '61':
+    curr_name = 'Dolar'
 
-if os.path.isfile('./CotacoesEuro2022.csv'):
-    #print('File exist')
+if os.path.isfile(f'./Cotacoes{curr_name}2022.csv'):
     cur_csv = r'C:\Users\miche\diretorio\WScrapingFXBacen\CotacoesEuro2022.csv'
     readfile_df = pd.read_csv(cur_csv, header= None, sep= ';')
     """Converter datas para date time antes de manipular"""
@@ -14,33 +18,31 @@ if os.path.isfile('./CotacoesEuro2022.csv'):
     #date = str(last_date.iloc[-1])[:-6] + '/' + str(last_date.iloc[-1])[-6:-4] + '/' + str(last_date.iloc[-1])[-4:]
     dt_inicio = f"{str(last_date.iloc[-1])[:-6]}/{str(last_date.iloc[-1])[-6:-4]}/{str(last_date.iloc[-1])[-4:]}"
     dt_inicio = datetime.strptime(dt_inicio, '%d/%m/%Y') + timedelta(1)
+    dt_inicio = str(dt_inicio)[:-9]
+    dt_inicio = f"{dt_inicio[8:]}/{dt_inicio[5:7]}/{dt_inicio[:4]}"
     dt_final = datetime.strftime(datetime.now() - timedelta(1), '%d/%m/%Y')
-    """Tirar horario da data inicio"""
-    #currency = input('Digite o código da moeda (USD: 61 ou EUR: 222): ')
-    #cur_csv = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
+    cur_csvnew = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
+    readnewfile_df = pd.read_csv(cur_csvnew, header= None, sep= ';')
+    print(readnewfile_df)
 
-    print(dt_inicio)
     """Buscar tail da coluna '0' adicionar 1 dia. Para isso, antes converter datas com Datetime"""
 else:
     print('File does not exist')
 
-url_download = "/ptax_internet/consultaBoletim.do?method=gerarCSVFechamentoMoedaNoPeriodo&ChkMoeda=61&DATAINI=08/02/2022&DATAFIM=07/03/2022"
-url_base = url_download.split('ChkMoeda')[0]
-currency = input('Digite o código da moeda (USD: 61 ou EUR: 222): ')
-dt_inicio = input('Digite a data inicial (dd/mm/aaaa): ')
-dt_final = input('Digite a data final (dd/mm/aaaa): ')
-#print(f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}')
-cur_csv = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
-readfile_df = pd.read_csv(cur_csv, header= None, sep= ';')
-print(readfile_df)
-
-
+#url_download = "/ptax_internet/consultaBoletim.do?method=gerarCSVFechamentoMoedaNoPeriodo&ChkMoeda=61&DATAINI=08/02/2022&DATAFIM=07/03/2022"
+#url_base = url_download.split('ChkMoeda')[0]
+#currency = input('Digite o código da moeda (USD: 61 ou EUR: 222): ')
+#dt_inicio = input('Digite a data inicial (dd/mm/aaaa): ')
+#dt_final = input('Digite a data final (dd/mm/aaaa): ')
+##print(f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}')
+#cur_csv = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
+#readfile_df = pd.read_csv(cur_csv, header= None, sep= ';')
+#print(readfile_df)
 
 """Próximos passos"""
-"""1- verificar se há base existente 'CotacoesEuro2022' """
+"""1- verificar se há base existente 'CotacoesEuro2022' """ """OK"""
 """2- criar base manualmente"""
 """3- rodar script todo e colar dados adicionais na base"""
 """4- Usar datetime para converter para data (DATETIME) e buscar link a partir da próxima data"""
-"""Usar tail na coluna para identificar ultima data extraída"""
 
 
