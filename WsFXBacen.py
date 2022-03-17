e8d4493f29bc0d6a4f341bbfae5b54c6c738544b
michelvasconcelos <michelvasconcelos@hotmail.com>
2022-03-17 10:16:09 -0300

    Leitura e Criação de arquivo não existente e adição de Headers

--- WsFXBacen.py
+++ WsFXBacen.py
@@ -1,3 +1,4 @@
+from email.header import Header
 import pandas as pd
 import os
 from datetime import datetime, timedelta
@@ -11,7 +12,7 @@ elif currency == '61':
     curr_name = 'Dolar'
 
 if os.path.isfile(f'./Cotacoes{curr_name}2022.csv'):
-    cur_csv = r'C:\Users\miche\diretorio\WScrapingFXBacen\CotacoesEuro2022.csv'
+    cur_csv = r'C:\Users\miche\diretorio\WScrapingFXBacen\Cotacoes' + curr_name + '2022.csv'
     readfile_df = pd.read_csv(cur_csv, header= None, sep= ';')
     """Converter datas para date time antes de manipular"""
     last_date = readfile_df[0].tail(1)
@@ -23,20 +24,27 @@ if os.path.isfile(f'./Cotacoes{curr_name}2022.csv'):
     dt_final = datetime.strftime(datetime.now() - timedelta(1), '%d/%m/%Y')
     cur_csvnew = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
     readnewfile_df = pd.read_csv(cur_csvnew, header= None, sep= ';')
-    #readfile_df = readfile_df.append(readnewfile_df)
-    #print(readfile_df)
-    readnewfile_df.to_csv(f'./Cotacoes{curr_name}2022.csv', mode= 'a', sep=';', index= False, header= False)
+    
+    readnewfile_df.to_csv(f'./Cotacoes{curr_name}2022.csv', mode= 'a', sep= ';', index= False, header= False)
 
     """Próximo passo criar .csv caso ele não exista"""
     """Passo seguinte criar 'temporizador para rodar o código todo dia após determinada hora"""
 else:
-    print('File does not exist')
+    dt_inicio = input('Digite a data inicial (dd/mm/aaaa): ')
+    dt_final = input('Digite a data final (dd/mm/aaaa): ')
+    cur_csvnew = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
+    print(cur_csvnew)
+    readnewfile_df = pd.read_csv(cur_csvnew, header= None, sep= ';', names= ['Data', 'Codigo da moeda', 'Nao sei', 'Moeda', 'Taxa compra', 'Taxa venda', 'Paridade dolar compra', 'Paridade dolar venda'])
+    readnewfile_df.to_csv(f'./Cotacoes{curr_name}2022.csv', sep= ';', index= False, header= True)
+
+    
+    
+    
+    #print('File does not exist')
 
 #url_download = "/ptax_internet/consultaBoletim.do?method=gerarCSVFechamentoMoedaNoPeriodo&ChkMoeda=61&DATAINI=08/02/2022&DATAFIM=07/03/2022"
 #url_base = url_download.split('ChkMoeda')[0]
 #currency = input('Digite o código da moeda (USD: 61 ou EUR: 222): ')
-#dt_inicio = input('Digite a data inicial (dd/mm/aaaa): ')
-#dt_final = input('Digite a data final (dd/mm/aaaa): ')
 ##print(f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}')
 #cur_csv = f'https://ptax.bcb.gov.br{url_base}ChkMoeda={currency}&DATAINI={dt_inicio}&DATAFIM={dt_final}'
 #readfile_df = pd.read_csv(cur_csv, header= None, sep= ';')
